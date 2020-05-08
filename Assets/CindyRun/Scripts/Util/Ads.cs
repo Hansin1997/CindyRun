0017c085cc893269a7a964f0e5b3900631c022fd
Hansin1997 <845612500@qq.com>
2020-05-08 17:10:42 +0800

    Unity Ads

---
+++ Assets/CindyRun/Scripts/Util/Ads.cs
@@ -0,0 +1,43 @@
+﻿using Cindy.Logic;
+using Cindy.Logic.ReferenceValues;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.Events;
+
+namespace CindyRun.Util
+{
+    public class Ads : LogicNode
+    {
+
+        public ReferenceString appStoreGameId;
+        public ReferenceString googlePlayGameId;
+
+        public ReferenceString placementId;
+
+        public ReferenceBool test;
+
+        public LogicNode success, fail,skip;
+        
+        protected void Start()
+        {
+            string gid = Application.platform == RuntimePlatform.Android ? googlePlayGameId.Value : appStoreGameId.Value;
+            Advertisement.Initialize(gid, test.Value);
+        }
+
+        protected override void Run()
+        {
+            ShowOptions o = new ShowOptions();
+            o.resultCallback = (s) =>
+            {
+                if (s == ShowResult.Finished)
+                    success.Execute();
+                if (s == ShowResult.Failed)
+                    fail.Execute();
+                else if(s == ShowResult.Skipped)
+                    skip.Execute();
+
+            };
+            Advertisement.Show(placementId.Value, o);
+        }
+    }
+}
---
+++ Assets/CindyRun/Scripts/Util/AdsReady.cs
@@ -0,0 +1,16 @@
+﻿using Cindy.Logic;
+using UnityEngine.Advertisements;
+using Cindy.Logic.ReferenceValues;
+
+namespace CindyRun.Util
+{
+    public class AdsReady : Condition
+    {
+        public ReferenceString placementId;
+
+        public override bool Check()
+        {
+            return Advertisement.IsReady(placementId.Value);
+        }
+    }
+}
